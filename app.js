78b1e869abcb717b61a1ee178204614d3e17e3b9
patrice prosper <new>
2017-03-14 05:46:13 -0400

    presentable

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(logger('dev'));
@@ -32,7 +31,6 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', index);
 app.use('/about', about);
 app.use('/projects', projects);
